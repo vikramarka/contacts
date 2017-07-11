dbd518715ad36ebc2be284bc0c5787b0dcdff245
vikram <kumar@currentlabel.co.uk>
2017-07-11 21:46:29 +0530

    Adding search bar

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,23 +1,43 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class ListContacts extends Component{
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+  state = {
+    query:""
+  }
+  updateQuery = (queryString)=>{
+    this.setState({query:queryString.trim()})
+  }
   render(){
-    console.log(this.props);
     return (
-      <ol className="list-contacts">
-        {this.props.contacts.map((contact)=>(
-          <li key={contact.id} className="contact-list-item">
-            <div className="contact-avatar" style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}/>
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <div className="contact-remove" />
-          </li>
-        ))}
-      </ol>
+      <div className="contact-list">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            value={this.state.query}
+            onChange={(event)=>this.updateQuery(event.target.value)}
+            placeholder="Search contacts"
+          />
+        </div>
+        <ol className="list-contacts">
+          {this.props.contacts.map((contact)=>(
+            <li key={contact.id} className="contact-list-item">
+              <div className="contact-avatar" style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}/>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <div onClick={()=>this.props.onDeleteContact(contact)} className="contact-remove" />
+            </li>
+          ))}
+        </ol>
+      </div>
     );
   }
 }
