1f2904452261bede4f152b15d05f2a0754fd2535
vikram <kumar@currentlabel.co.uk>
2017-07-12 22:02:12 +0530

    Displaying count for search results

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   }
   removeContact = (contact)=>{
     this.setState((state)=>({
-      contacts: state.contacts.filter((c) => c.id!=contact.id)
+      contacts: state.contacts.filter((c) => c.id!==contact.id)
     }));
   }
   render() {
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
 class ListContacts extends Component{
   static propTypes = {
@@ -12,19 +14,42 @@ class ListContacts extends Component{
   updateQuery = (queryString)=>{
     this.setState({query:queryString.trim()})
   }
+  showAll = () => {
+    this.setState({query:''});
+  }
   render(){
+
+    const {contacts, onDeleteContact} = this.props;
+    const {query} = this.state;
+
+    let searchedContacts;
+    if(query){
+      let match = new RegExp(escapeRegExp(query),"i");
+      searchedContacts = contacts.filter((contact)=>match.test(contact.name))
+    }
+    else{
+      searchedContacts = contacts;
+    }
+    searchedContacts = searchedContacts.sort(sortBy("name"));
     return (
-      <div className="contact-list">
+      <div className="list-contacts">
         <div className="list-contacts-top">
           <input
             className="search-contacts"
-            value={this.state.query}
+            value={query}
             onChange={(event)=>this.updateQuery(event.target.value)}
             placeholder="Search contacts"
           />
         </div>
-        <ol className="list-contacts">
-          {this.props.contacts.map((contact)=>(
+        {searchedContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>Now showing {searchedContacts.length} of {contacts.length} total</span>
+            <button onClick={this.showAll}>Show all</button>
+          </div>
+          )
+        }
+        <ol className="contact-list">
+          {searchedContacts.map((contact)=>(
             <li key={contact.id} className="contact-list-item">
               <div className="contact-avatar" style={{
                 backgroundImage: `url(${contact.avatarURL})`
@@ -33,7 +58,7 @@ class ListContacts extends Component{
                 <p>{contact.name}</p>
                 <p>{contact.email}</p>
               </div>
-              <div onClick={()=>this.props.onDeleteContact(contact)} className="contact-remove" />
+              <div onClick={()=>onDeleteContact(contact)} className="contact-remove" />
             </li>
           ))}
         </ol>
